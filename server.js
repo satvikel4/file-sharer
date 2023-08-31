7bfd8fe9d90c6add623477241fb3d81b0ee195f8
satvikel4 <56204066+satvikel4@users.noreply.github.com>
2023-08-31 02:30:05 -0700

    file upload

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express")
+const req = require("express/lib/request")
 const app = express()
 
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.post("/upload", (req, res) => {
+    res.send("hi")
 })
 
 app.listen(3000)
