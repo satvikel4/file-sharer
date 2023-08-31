a8bcc39a227cf6ffe75ffa31705d1bf5ac099f3d
satvikel4 <56204066+satvikel4@users.noreply.github.com>
2023-08-31 03:20:22 -0700

    password protection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const File = require("./models/File")
 const express = require("express")
 const req = require("express/lib/request")
 const app = express()
-
+app.use(express.urlencoded({ extended: true }))
 const upload = multer({ dest: "uploads" })
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -29,12 +29,24 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 }) 
 
-app.get("/file/:id", async (req, res) => {
+app.route("/file/:id").get(handleDownload).post(handleDownload)
+
+async function handleDownload(req, res) {
     const file = await File.findById(req.params.id)
+    if (file.password != null) {
+        if (req.body.password == null) {
+            res.render("password")
+            return
+        }
+        if (!await bcrypt.compare(req.body.password, file.password)) {
+            res.render("password", { error: true })
+            return
+        }
+    }
     file.downloadCount++
     await file.save()
     console.log(file.downloadCount)
     res.download(file.path, file.originalName)
-})
+}
 
 app.listen(process.env.PORT)  
