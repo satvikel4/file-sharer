6607b66d7702e0ea18ec95ee2c4dde355211b537
satvikel4 <56204066+satvikel4@users.noreply.github.com>
2023-08-31 03:07:34 -0700

    file download server logic

--- server.js
+++ server.js
@@ -26,8 +26,15 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         fileData.password = await bcrypt.hash(req.body.password, 10)
     }
     const file = await File.create(fileData)
-    console.log(file)
-    res.send(file.originalName)
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 }) 
+
+app.get("/file/:id", async (req, res) => {
+    const file = await File.findById(req.params.id)
+    file.downloadCount++
+    await file.save()
+    console.log(file.downloadCount)
+    res.download(file.path, file.originalName)
+})
 
 app.listen(process.env.PORT)  
